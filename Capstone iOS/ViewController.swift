ae415ea10794dc24ad53235df44ae7a6c8e2c300
leomc1118 <classyleo1800@gmail.com>
2025-10-08 10:13:58 -0400

    Initial Commit

---
+++ Capstone iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Capstone iOS
+//
+//  Created by Leo Chen on 10/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
